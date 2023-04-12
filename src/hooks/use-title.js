185f35b9e4c5176c18cd133720b89643ec1b9141
theperfesser <perfesser100@gmail.com>
2023-04-11 19:54:10 -0600

    update document.title

--- src/hooks/use-title.js
+++ src/hooks/use-title.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 export function useTitle(title) {
   useEffect(() => {
     // const prevTitle = document.title
-    document.title = title ? `${title} - teia` : 'teia'
+    document.title = title ? `${title} | spam-et-nunc` : 'spam-et-nunc'
     // return () => {
     //   console.log(`exiting ${prevTitle}`)
     //   document.title = prevTitle
